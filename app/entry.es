acb98278c6dd0e6d7721160af1d2f468d894a9b5
Wes Duff <wduff@W-DN-ML-WDUFF.local>
2017-07-10 14:54:44 -0600

    addition of postCSS and POSTCSS-NEXT

---
+++ app/entry.es
@@ -0,0 +1,4 @@
+import URI from '../tools/dist/app_config'
+import styles from './public/css/app.css'
+
+console.dir(URI);
---
+++ webpack.config.js
@@ -0,0 +1,117 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+  entry: "./app/entry.es", // string | object | array
+  // Here the application starts executing
+  // and webpack starts bundling
+
+  output: {
+    // options related to how webpack emits results
+
+    path: path.resolve(__dirname, "app/public/js/bundle"), // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+
+    filename: "bundle.js", // string
+    // the filename template for entry chunks
+
+    publicPath: "/assets/", // string
+    // the url to the output directory resolved relative to the HTML page
+  },
+
+  module: {
+    // configuration regarding modules
+
+    rules: [
+      // rules for modules (configure loaders, parser options, etc.)
+      {
+        test: /\.(es|jsx?)$/,
+        exclude: [/node_modules/, /tools/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015', 'es2016', 'es2017'] },
+        }],
+      },
+
+
+       {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                { loader: 'css-loader', query: { modules: true, sourceMaps: true } },
+                'postcss-loader'
+            ]
+        })
+      }
+
+
+    ]
+    /* Advanced module configuration (click to show) */
+  },
+  resolve: {
+    // options for resolving module requests
+    // (does not apply to resolving to loaders)
+
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "app")
+    ],
+    // directories where to look for modules
+
+    extensions: [".js", ".es", ".json", ".jsx", ".css"],
+    // extensions that are used
+  },
+
+  performance: {
+    hints: "warning", // enum
+    maxAssetSize: 200000, // int (in bytes),
+    maxEntrypointSize: 400000, // int (in bytes)
+    assetFilter: function(assetFilename) {
+      // Function predicate that provides asset filenames
+      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+    }
+  },
+
+  target: "web", // enum
+  // the environment in which the bundle should run
+  // changes chunk loading behavior and available modules
+
+  externals: ["react"],
+  // Don't follow/bundle these modules, but request them at runtime from the environment
+
+  stats: "errors-only",
+  // lets you precisely control what bundle information gets displayed
+
+  devServer: {
+    proxy: { // proxy URLs to backend development server
+      '/api': 'http://localhost:3000'
+    },
+    contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
+    compress: true, // enable gzip compression
+    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    https: false, // true for self-signed, object for cert authority
+    noInfo: true, // only errors & warns on hot reload
+    // ...
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+        options: {
+            context: path.root,
+            postcss: [ // <---- postcss configs go here under LoadOptionsPlugin({ options: { ??? } })
+                require('postcss-cssnext'),
+                require('postcss-reporter')()
+            ]
+            // ...other configs that used to directly on `modules.exports`
+        }
+    }),
+    new ExtractTextPlugin({ filename: '../../css/bundle/[name].css', disable: false, allChunks: true })
+  ],
+  // list of additional plugins
+
+
+  /* Advanced configuration (click to show) */
+}
