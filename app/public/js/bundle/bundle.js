935644bf2a28e83f686f40fcb9183453bd8d7eaf
Danny Robinson <drobinson@healthgrades.com>
2017-07-11 12:57:23 -0600

    Storybook added to project w/ default configs

--- app/public/js/bundle/bundle.js
+++ app/public/js/bundle/bundle.js
@@ -57,7 +57,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/assets/";
+/******/ 	__webpack_require__.p = "app/public";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
@@ -70,11 +70,11 @@
 "use strict";
 
 
-var _app_config = __webpack_require__(1);
+var _app_config = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"../tools/dist/app_config\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 
 var _app_config2 = _interopRequireDefault(_app_config);
 
-var _app = __webpack_require__(2);
+var _app = __webpack_require__(1);
 
 var _app2 = _interopRequireDefault(_app);
 
@@ -84,29 +84,6 @@ console.dir(_app_config2.default);
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var URI = {
-  baseUISVC_URI: "/uisvc/v1_0/oas-bp-uisvc/api/"
-};
-
-var PORTS = {
-  localhostPortNumber_uisvc: "9705"
-};
-
-exports.default = URI;
-exports.URI = URI;
-exports.PORTS = PORTS;
-
-
-/***/ }),
-/* 2 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
