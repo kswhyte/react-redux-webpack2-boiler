562860da7076b0bc1f78de9d5606bd5840f421ae
Wes Duff <wduff@W-DN-ML-WDUFF.local>
2017-07-13 14:12:56 -0600

    moving app inside .storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
 import { configure } from '@kadira/storybook';
-import '../app/assets/css/bootstrap.min.css';
+import './app/assets/css/bootstrap.min.css';
 
 const req = require.context('../src/components/', true, /(_test|_spec)\.jsx$/);
 
