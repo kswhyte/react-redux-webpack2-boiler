bb1083a626b5c51c907aba960362ecfe7c1df051
Danny Robinson <drobinson@healthgrades.com>
2017-07-18 15:23:28 -0600

    Added Login, SearchPatient, UserManagement, adjusted css file extenstions

---
+++ src/components/SearchPatient/SearchPatient.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './search-patient.css';
+
+const SearchPatient = () => {
+    return (
+        <div className="search-patient-block">
+            <h1>Search Patient</h1>
+        </div>
+    )
+}
+
+SearchPatient.propTypes = {};
+
+export default SearchPatient;
