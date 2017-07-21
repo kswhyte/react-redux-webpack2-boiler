67d64aaa6beddac28b2afa04ea0e8ec9a22dd95d
Kinan Whyte <kwhyte@healthgrades.com>
2017-07-21 16:22:22 -0600

    Add layout and styling for icons for dashboard and setting icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import './header.css';
 
@@ -8,18 +9,31 @@ const Header = props => {
     headerSize: PropTypes.string
   };
 
+  //handle no margin getting overwritten?
   return (
     <header className={props.headerSize === 'small' ? 'small' : null}>
-      <div>
-        <div className="logo-window">
+      <div className="row no-margin">
+        <div className="logo-window col-xs-6">
           <div className="logo-animation">
             <div className="logo-full" />
             <div className="logo-small" />
           </div>
         </div>
-        <i className="fa fa-tachometer fa-2" aria-hidden="true" />
-        <i className="fa fa-cog fa-2" aria-hidden="true" />
+
+        <div className="col-xs-6">
+          <div className="pull-right">
+            <Link to="/">
+              <i className="fa fa-tachometer fa-2" aria-hidden="true" />
+              <div className="text-small-o">Dashboard</div>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-cog fa-2" aria-hidden="true" />
+              <div className="text-small-o">Settings</div>
+            </Link>
+          </div>
+        </div>
       </div>
+
       <div>
         <div>MR Metropolitan Region</div>
       </div>
