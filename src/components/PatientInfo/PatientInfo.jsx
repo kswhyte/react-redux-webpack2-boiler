c0e315cd65e4fcd8b7089aa97885048ce2917bb6
Kinan Whyte <kwhyte@healthgrades.com>
2017-07-28 16:23:12 -0600

    Create Form elements for PatientInfo

--- src/components/PatientInfo/PatientInfo.jsx
+++ src/components/PatientInfo/PatientInfo.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// import { HgReform } from '@hg/three-ui/HgReform';
+import HgRow from '@hg/three-ui/HgRow';
+import HgCol from '@hg/three-ui/HgCol';
+
+import TextInput from '@hg/three-ui/HgInputsV2/TextInput';
+import SelectInput from '@hg/three-ui/HgInputsV2/SelectInput';
+// import MaskedTextInput from '../ExtraFormElements/maskedInputs';
+// import HgButton from '@hg/three-ui/HgButton';
+
+import './patient-info.css';
+
 const PatientInfo = props => {
   PatientInfo.propTypes = {
     children: PropTypes.object
@@ -15,9 +26,103 @@ const PatientInfo = props => {
     <div className="patient-info-wrapper">
       <div className="tab-content clearfix">
         <div className="general-info-form tab-pane active">
-          <h3>Insert GENERAL INFO Form</h3>
-          <form onSubmit={handlePatientInfoSubmit}>
-            <input />
+          <hr />
+          <form>
+            <HgRow>
+              <div className="col-sm-3">
+                <TextInput label="PATIENT FIRST NAME &#42;" placeholder="Patient First Name" />
+              </div>
+
+              <div className="col-sm-4">
+                <TextInput label="PATIENT LAST NAME &#42;" placeholder="Patient Last Name" />
+              </div>
+
+              <div className="col-sm-2">
+                <SelectInput
+                  label="GENDER &#42;"
+                  required
+                  options={[{ id: 0, text: 'Male' }, { id: 1, text: 'Female' }]}
+                />
+              </div>
+
+              <div className="col-sm-3">
+                <TextInput label="EMAIL ADDRESS &#42;" placeholder="Email Adress" />
+              </div>
+            </HgRow>
+
+            <HgRow>
+              <div className="col-sm-3">
+                <SelectInput label="PATIENT DATE OF BIRTH &#42;" required />
+              </div>
+
+              <div className="col-sm-2">
+                <SelectInput label="PATIENT DATE OF BIRTH &#42;" required />
+              </div>
+
+              <div className="col-sm-2">
+                <SelectInput label="PATIENT DATE OF BIRTH &#42;" required />
+              </div>
+
+              <div className="col-sm-2">
+                <TextInput label="PHONE NUMBER &#42;" placeholder="Phone Number" />
+              </div>
+
+              <div className="col-sm-3">
+                <SelectInput label="CONTACT METHOD &#42;" required />
+              </div>
+            </HgRow>
+
+            <HgRow>
+              <div className="col-sm-4">
+                <TextInput label="STREET &#42;" required />
+              </div>
+
+              <div className="col-sm-3">
+                <TextInput label="CITY STATE ZIP &#42;" required />
+              </div>
+
+              <div className="col-sm-2">
+                <SelectInput label="CITY STATE ZIP &#42;" required />
+              </div>
+
+              <div className="col-sm-2">
+                <TextInput label="CITY STATE ZIP &#42;" placeholder="Zip" required />
+              </div>
+            </HgRow>
+
+            <HgRow>
+              <div className="col-sm-4">
+                <SelectInput label="PRIMARY INSURANCE CARRIER &#42;" required />
+              </div>
+
+              <div className="col-sm-2">
+                <SelectInput label="PLAN TYPE &#42;" required />
+              </div>
+
+              <div className="col-sm-3">
+                <TextInput label="PLAN NO. &#42;" placeholder="Plan Number" required />
+              </div>
+            </HgRow>
+
+            <HgRow>
+              <div className="col-sm-4">
+                <SelectInput label="ALTERNATIVE INSURANCE CARRIER &#42;" required />
+              </div>
+
+              <div className="col-sm-2">
+                <SelectInput label="PLAN TYPE &#42;" required />
+              </div>
+
+              <div className="col-sm-3">
+                <TextInput label="PLAN NO. &#42;" placeholder="Plan Number" required />
+              </div>
+            </HgRow>
+
+            <HgRow>
+              <div className="col-sm-4">
+                <SelectInput label="REFERRING PHYSICIAN &#42;" required />
+              </div>
+            </HgRow>
           </form>
         </div>
       </div>
