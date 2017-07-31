ce750710832b09b707d7c52c89d5c88c03f5deb5
scneti\svenkatachalam <svenkatachalam@healthgrades.com>
2017-07-31 19:20:23 +0530

    PatientInfo css and test updates

--- src/components/SearchPatient/SearchPatient.jsx
+++ src/components/SearchPatient/SearchPatient.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './search-patient.css';
 
+
 const SearchPatient = () => {
     return (
         <div className="search-patient-block">
             <h2>Patient Information</h2>
+            <hr className="line"/>
         </div>
     )
 }
