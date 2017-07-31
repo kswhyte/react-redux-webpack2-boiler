1609a3b3aa2300c3b94e5823918cd3d9cd94dff1
Danny Robinson <drobinson@healthgrades.com>
2017-07-31 16:29:18 -0600

    Completed basic html styling

--- src/components/SignOn/SignOn.jsx
+++ src/components/SignOn/SignOn.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 
-import HgRow from '@hg/three-ui/HgRow';
 import TextInput from '@hg/three-ui/HgInputsV2/TextInput';
 import HgButton from '@hg/three-ui/HgButton';
 
@@ -18,11 +17,14 @@ const propTypes = {
 const SignOn = (props) => {
   return (
     <div className="sign-on-wrapper">
-      <h2>SIGN ON</h2>
-      <h3>Please enter your email address and password below.</h3>
-
-      <form
-        onSubmit={(e) => {
+        <div className="row sm-spacer">
+            <h2>SIGN ON</h2>
+            <div className="page-instructions col-xs-offset-4 col-xs-4">
+                <h4>Please enter your email address and password below.</h4>
+            </div>
+        </div>
+        <form
+            onSubmit={(e) => {
                 e.preventDefault();
                 const loginEmail = document.getElementById("loginEmail").value;
                 const loginPassword = document.getElementById("loginPassword").value;
@@ -33,38 +35,48 @@ const SignOn = (props) => {
                 props.submitLogin(data);
             }}
         >
-        <HgRow>
-          <div className="col-sm-6">
-               <TextInput
-                    id="loginEmail"
-                    className="form-group"
-                    placeholder={'Your Email Address'}
-                    label="User Name (Email)"
-                    required={true}
-                />
-          </div>
-          <div className="col-sm-6">
-            <TextInput
-                id="loginPassword"
-                className="form-group"
-                placeholder={'Password'}
-                label="Your Password"
-                required={true}
-            />
-        </div>
-        </HgRow>
-            <HgButton
-                className="primary" text="Sign On"
-            />
-            <label>Remember Me?</label>
-            <input
-                onClick={() => {console.log("Remember Me")}}
-                id="checkBox"
-                type="checkbox" />
-
+            <div className="row md-spacer">
+                <div className="col-sm-6">
+                   <TextInput
+                        id="loginEmail"
+                        className="form-group"
+                        placeholder={'Your Email Address'}
+                        label="User Name (Email)"
+                        required={true}
+                    />
+                </div>
+                <div className="col-sm-6">
+                    <TextInput
+                        id="loginPassword"
+                        className="form-group"
+                        placeholder={'Your Password'}
+                        label="Your Password"
+                        required={true}
+                    />
+                </div>
+            </div>
+            <div className="row lg-spacer">
+                <div className="col-xs-offset-3 col-xs-6 flex-column">
+                    <HgButton
+                        className="primary"
+                        text="Sign On"
+                    />
+                    <div className="row sm-spacer">
+                        <div
+                            className="remember-checkbox">
+                            <div className="checkaroo">
+                                <i className="fa fa-check" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <label>Remember Me?</label>
+                    </div>
+                    <div className="row md-spacer forgot-password">
+                        <Link to="/login/resetpassword">Forgot your password?</Link>
+                        <Route exact path="/login/resetpassword" component={ResetPassword} />
+                    </div>
+                </div>
+            </div>
         </form>
-        <Link to="/login/resetpassword">Forgot your password?</Link>
-        <Route exact path="/login/resetpassword" component={ResetPassword} />
     </div>
   );
 };
