f1cde981f42d5f0ae71c6e4ccb5f60a85909f37f
Kinan Whyte <kwhyte@healthgrades.com>
2017-08-03 16:35:48 -0600

    merge updates to PatientAppointments, add css styling, and jsx for PatientNotes

--- src/components/PatientNotes/PatientNotes.jsx
+++ src/components/PatientNotes/PatientNotes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
 
 const PatientNotes = props => {
   PatientNotes.propTypes = {
@@ -11,6 +12,10 @@ const PatientNotes = props => {
   };
   check();
 
+  // const existingNotes = props.appointments.filter(note => {
+  //   return Moment(note.noteDateTime) < Moment();
+  // });
+
   return (
     <div className="patient-notes-wrapper">
       <div className="tab-content clearfix">
@@ -21,33 +26,34 @@ const PatientNotes = props => {
             <th>Title</th>
             <th>Actions</th>
           </table>
-          {existingNotes.length > 0 && <ExistingNotes existingNotes={existingNotes} />}
+          {/* {existingNotes.length > 0 && <ExistingNotes existingNotes={existingNotes} />} */}
         </div>
       </div>
     </div>
   );
 };
 
-const ExistingNotes = props => {
-  ExistingNotes.propTypes = {
-    existingNotes: PropTypes.array.required
-  };
-  return (
-    <table className="notes-tables">
-      {props.existingNotes.map(note => {
-        return (
-          <tr key={note.noteID}>
-            <td>
-              {Moment(note.noteDateTime).format('LL')}
-            </td>
-            <td>
-              {Moment(note.noteTitle).format('LT')}
-            </td>
-          </tr>
-        );
-      })}
-    </table>
-  );
-};
+//TODO: make sure data is coming in for each note. UNCOMMENT when Redux wiring is setup
+// const ExistingNotes = props => {
+//   ExistingNotes.propTypes = {
+//     existingNotes: PropTypes.array.required
+//   };
+//   return (
+//     <table className="notes-tables">
+//       {props.existingNotes.map(note => {
+//         return (
+//           <tr key={note.noteID}>
+//             <td>
+//               {Moment(note.noteDateTime).format('LL')}
+//             </td>
+//             <td>
+//               {Moment(note.noteTitle).format('LT')}
+//             </td>
+//           </tr>
+//         );
+//       })}
+//     </table>
+//   );
+// };
 
 export default PatientNotes;
