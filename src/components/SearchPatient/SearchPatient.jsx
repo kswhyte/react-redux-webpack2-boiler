e559ffc1bef3044216db30a6f5c6c13b4cab911f
Danny Robinson <drobinson@healthgrades.com>
2017-08-15 13:59:33 -0600

    Added basic layout

--- src/components/SearchPatient/SearchPatient.jsx
+++ src/components/SearchPatient/SearchPatient.jsx
@@ -7,6 +7,16 @@ const SearchPatient = () => {
     <div className="search-patient-block">
       <h2>Patient Information</h2>
       <hr className="line" />
+      <label htmlFor="firstName">First Name</label>
+      <input type="text" id="firstName" />
+      <label htmlFor="lastName">Last Name</label>
+      <input type="text" id="lastName" />
+      <label htmlFor="dateOfBirth">Date of Birth</label>
+      <input type="text" id="dateOfBirth" />
+      <label htmlFor="phone">Phone</label>
+      <input type="text" id="phone" />
+      <label htmlFor="zipCode">Zip Code</label>
+      <input type="text" id="zipCode" />
     </div>
   );
 };
