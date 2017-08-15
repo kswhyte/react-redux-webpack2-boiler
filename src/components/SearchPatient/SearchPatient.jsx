ae2b8fde7684ce36038e7a0be3eeba27e7307ebf
Danny Robinson <drobinson@healthgrades.com>
2017-08-15 14:30:40 -0600

    Main JSX setup, basic CSS

--- src/components/SearchPatient/SearchPatient.jsx
+++ src/components/SearchPatient/SearchPatient.jsx
@@ -5,18 +5,37 @@ import './search-patient.css';
 const SearchPatient = () => {
   return (
     <div className="search-patient-block">
-      <h2>Patient Information</h2>
-      <hr className="line" />
-      <label htmlFor="firstName">First Name</label>
-      <input type="text" id="firstName" />
-      <label htmlFor="lastName">Last Name</label>
-      <input type="text" id="lastName" />
-      <label htmlFor="dateOfBirth">Date of Birth</label>
-      <input type="text" id="dateOfBirth" />
-      <label htmlFor="phone">Phone</label>
-      <input type="text" id="phone" />
-      <label htmlFor="zipCode">Zip Code</label>
-      <input type="text" id="zipCode" />
+        <h2>Patient Information</h2>
+        <hr className="line" />
+        <form>
+            <div className="row" >
+                <div className="col-sm-5">
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName" />
+                </div>
+                <div className="col-sm-5">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName" />
+                </div>
+            </div>
+            <div className="row sm-spacer">
+                <div className="col-sm-3">
+                    <label htmlFor="dateOfBirth">Date of Birth</label>
+                    <input type="text" id="dateOfBirth" />
+                </div>
+                <div className="col-sm-3">
+                    <label htmlFor="phone">Phone</label>
+                    <input type="text" id="phone" />
+                </div>
+                <div className="col-sm-3">
+                    <label htmlFor="zipCode">Zip Code</label>
+                    <input type="text" id="zipCode" />
+                </div>
+                <div className="col-sm-3">
+                    <button>Find Patient Record</button>
+                </div>
+            </div>
+        </form>
     </div>
   );
 };
